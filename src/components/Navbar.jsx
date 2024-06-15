580c4a819508e19e66f18c8814a5e8cb62d5ee94
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-06-14 23:05:04 -0500

    24:14

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <nav
       className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div></div>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto"></div>
     </nav>
   );
 };
