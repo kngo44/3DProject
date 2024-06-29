7e1af159628232ce5b32b64edf6889019e371c2e
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-06-28 21:19:34 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link>htrshsrt</Link>
+        <Link></Link>
       </div>
     </nav>
   );
